75482d2763e83c4b450010490971a96f2d413c48
AxelJosephYang <axeljyang@gmail.com>
2021-12-11 14:38:46 +0700

    Commit pembayaran, tiket, points, location

---
+++ app/src/main/java/com/example/tubesp3b/PembayaranFragment.java
@@ -0,0 +1,75 @@
+package com.example.tubesp3b;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+
+import androidx.fragment.app.Fragment;
+
+import com.example.tubesp3b.databinding.FragmentPembayaranBinding;
+
+public class PembayaranFragment extends Fragment implements View.OnClickListener{
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+    private FragmentPembayaranBinding binding;
+
+    public PembayaranFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment BookNowFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static PembayaranFragment newInstance(String param1, String param2) {
+        PembayaranFragment fragment = new PembayaranFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        binding = FragmentPembayaranBinding.inflate(inflater);
+        View view = binding.getRoot();
+
+        binding.butPesan.setOnClickListener(this);
+
+        return view;
+    }
+
+    @Override
+    public void onClick(View v){
+        if(v == binding.butPesan){
+            Toast.makeText(getActivity(), "Order succeed", Toast.LENGTH_SHORT).show();
+        }
+    }
+}
+
